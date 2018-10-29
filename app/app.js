c4cfe1c654f4cffab83b8f8cae4ab9d665d7d8a9
Gustavo Silva <gustavo.silva@aptoide.com>
2018-10-29 10:54:12 +0000

    [new] added local dev to tests

--- app/app.js
+++ app/app.js
@@ -33,7 +33,7 @@ import configureStore from './configureStore';
 import { translationMessages } from './i18n';
 
 // Import CSS reset and Global Styles
-import './global-styles';
+import './styles/global-styles';
 
 // Create redux store with history
 const initialState = {};
--- app/utils/storage.js
+++ app/utils/storage.js
@@ -1,12 +1,24 @@
 const USER_SESSION = 'user_session';
 
+const isExtension = chrome && chrome.storage;
+
 //  Save data on the browser
 const save = (key, value, fn = () => null) => {
-  chrome.storage.local.set({ key: value }, fn);
+  if (isExtension) {
+    return chrome.storage.local.set({ [key]: value }, fn);
+  }
+
+  return window.localStorage.setItem([key], value) && fn;
 };
 
 //  Fetch data from the browser
-const get = (key, fn = () => null) => chrome.storage.local.get([key], fn);
+const get = (key, fn = () => null) => {
+  if (isExtension) {
+    return chrome.storage.local.get([key], fn);
+  }
+
+  return window.localStorage.getItem(key) && fn;
+};
 
 //  save userSession
 export const setUserSession = data => save(USER_SESSION, data);
