c554ce9a9b2b98e8652db25b152471df2965cf2b
Gustavo Silva <gustavo.silva@aptoide.com>
2018-11-15 17:28:20 +0000

    [new] added unit testing to conersions

---
+++ app/utils/tests/conversion.test.js
@@ -0,0 +1,54 @@
+/**
+ * Test injectors
+ */
+
+import {
+  convertSatsToUnit,
+  transSatToUnit,
+  convertFiatBtcToFiatUnit,
+} from '../conversion';
+import { BTC, MBTC, SAT } from '../constants';
+
+describe('conversion', () => {
+  it('should correctly convert 100000000 SAT to 1.0000 BTC', () => {
+    const value = 100000000; // 1 BTC
+    const expectedValue = '1.0000';
+    expect(convertSatsToUnit(value, BTC)).toBe(expectedValue);
+  });
+
+  it('should correctly transform 10 satoshis into amount and unit', () => {
+    const valueInSat = 10;
+    const expectedValue = { amount: '10', unit: SAT };
+    const result = transSatToUnit(valueInSat);
+    expect(result.amount).toBe(expectedValue.amount);
+    expect(result.unit).toBe(expectedValue.unit);
+  });
+
+  it('should correctly transform 989872 satoshis into amount and unit', () => {
+    const valueInSat = 989872;
+    const expectedValue = { amount: '9.8987', unit: MBTC };
+    const result = transSatToUnit(valueInSat);
+    expect(result.amount).toBe(expectedValue.amount);
+    expect(result.unit).toBe(expectedValue.unit);
+  });
+
+  it('should correctly transform 265342553 satoshis into amount and unit', () => {
+    const valueInSat = 265342553;
+    const expectedValue = { amount: '2.6534', unit: BTC };
+    const result = transSatToUnit(valueInSat);
+    expect(result.amount).toBe(expectedValue.amount);
+    expect(result.unit).toBe(expectedValue.unit);
+  });
+
+  it('should correctly convert 5643.20 USD/BTC into USD/MBTC and USD/SAT', () => {
+    const valueInBTC = 5643.2;
+
+    const expectedValueMBTC = 5.6432;
+    const resultMBTC = convertFiatBtcToFiatUnit(valueInBTC, MBTC);
+    expect(resultMBTC).toBe(expectedValueMBTC);
+
+    const expectedValueSAT = 0.000056432;
+    const resultSAT = convertFiatBtcToFiatUnit(valueInBTC, SAT);
+    expect(resultSAT).toBe(expectedValueSAT);
+  });
+});
