a8463ab8129dd45f1ace4e59974c8adb78803e47
Gustavo Silva <gustavo.silva@forkit.pt>
2018-11-24 21:44:00 +0000

    [new] decouple signup form

---
+++ app/components/SignUpForm/messages.js
@@ -0,0 +1,26 @@
+/*
+ * SignUpForm Messages
+ *
+ * This contains all the text for the SignUpForm component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.SignUpForm.header',
+    defaultMessage: 'This is the SignUpForm component !',
+  },
+  new_password: {
+    id: 'app.containers.SignUpForm.password',
+    defaultMessage: 'New Password (min 8 chars)',
+  },
+  confirm_password: {
+    id: 'app.containers.SignUpForm.password',
+    defaultMessage: 'Confirm Password',
+  },
+  submit: {
+    id: 'app.containers.SignUp.submit',
+    defaultMessage: 'Submit',
+  },
+});
